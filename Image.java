57b186e90d272b09d53e300d5a7a9d3344e8ab6d
bikramale99 <44412068+bikramale99@users.noreply.github.com>
2018-10-31 17:21:43 +0100

    many change done

--- Image.java
+++ Image.java
@@ -1,112 +1,56 @@
-import java.awt.Color;
-    
+import java.io.FileOutputStream;
+import java.io.IOException;
 
-    import java.io.FileNotFoundException;
-    import java.io.FileOutputStream;
-    import java.io.IOException;
-    import java.util.ArrayList;
+public class Image
+{
+	// byte array to hold the raw image data
 
+	public static int width;
+	public static int height;
+	public static byte  [] data;
+	static int index;
+	static String hexVal = "0x";
 
-
-public class Image {
-	private static  int height=0;
-	private static  int width=0;
-	public byte[] data;
-
-	private static ArrayList pic;
-	private static String matrix="";
-	
-	private static Color color;
-	private final static String fileName="Assignment1.ppm";
-	
-	
-	/*public static void main(String[] args) throws IOException {
-		
-		image(200,200);
-	    writeImage(fileName);	
-	
-		
-	}*/
-	
-	public Image(int height, int width) {
-		
-		this.height = height;
-		this.width = width;
-		this.data = new byte [height*width*3];
-		
-	}
-	
-	public static void Image(int width, int height){
-		pic=new ArrayList();
-		byte [] image=new byte[3];
-		matrix +="P3\n" + width + "\n" + height + "\n255\n";
-		
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				 setPixels(width,height,j,i);
-				
-				if(color==Color.red) {
-					image[0]=(byte)(255*factor(width,height,j,i));
-					image[1]=0;
-					image[2]=0;		
-				}else if (color==Color.green) {
-					image[0]=0;
-					image[1]=(byte)(255*factor(width,height,j,i));
-					image[2]=0;	
-				}else if (color==Color.blue) {
-					image[0]=0;
-					image[1]=0;
-					image[2]=(byte)(255*factor(width,height,j,i));
-					
-				}
-				matrix += ""+ image[0] + " " + image[1] + " " + image[2] + "  " ;
-				
-				
-			}
-			matrix += "\n";
-		}
-		
-		
-		
-	}
-	
-	public static void setPixels(int width,int height, int a,int b ) {
+//constructor to create image object with specified height and width	
+	public Image(int width, int height) {
+		Image.width = width;
+		Image.height = height;
+		data = new byte[3*height*width];
 			
-		double d1 = ((double) width / height) * a;
-        double d2 = (((double) -width / height) * a + height);
-        if(d1 > b && d2 > b) 
-        	{
-        	color= Color.green;
-        	}
-        else if(d1 > b && d2 < b)
-        {
-        	color= Color.blue;
-        	}
-        else {
-       
-        color= Color.red;
-        }
 	}
+//sets method to set the rgb values to single pixel
+public static void set(int x, int y, int val) {
+		String st = hexVal + Integer.toHexString(val) ;
+		System.out.println(st);
+		//3 bytes per pixel throughout the width
+		index = (y*width+x)*3;
+		int count=0; 
+		int sub1=2; 
+		int sub2=4;
+		//setting up the pixel position
+				while (count <= 2 )
+				 {
+					String substr = st.substring(sub1, sub2);
+					data [index + count] = (byte) Integer.parseInt(substr, 16);			//https://www.javamex.com/tutorials/conversion/decimal_hexadecimal.shtml
+					count++;
+					sub1 = sub2;
+					sub2 +=2;
+				}
 	
+}
+//writes image data into file
+// throws IOException for handling file writing errors
+public void write(String filename) throws IOException {
 	
-	public static double  factor(int width, int height, int a, int b){
-        double factorX = (double) Math.min(a, width - a) / width * 2;
-        double factorY = (double) Math.min(b, height - b) / height * 2;
-
-       
-        return Math.min(factorX, factorY);
-    }
-	
-	public static void write( String fn) throws FileNotFoundException, IOException{
-		FileOutputStream fos = new FileOutputStream(fn);
-        fos.write(new String(matrix).getBytes());
-        
-        fos.close();
-	}
-	
-
-
-	
-	
-
+	FileOutputStream out = new FileOutputStream(filename);
+	//writes header file for the image with specified height and width
+	byte[] header = ("P3\n" + width + "\n" + height + "\n255\n").getBytes();
+	out.write(header);
+	//Writes throughout the image for pixel value
+	for(int i=0; i<data.length; i++)
+			{
+				out.write(data[i]);
+			}
+	out.close();
+}
 }
