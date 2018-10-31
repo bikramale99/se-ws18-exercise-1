5b13f73baf2faf6acfd56fade076c3f52e134dfd
bikramale99 <44412068+bikramale99@users.noreply.github.com>
2018-10-31 21:03:31 +0100

    updates

--- Image.java
+++ Image.java
@@ -19,11 +19,11 @@ public class Image
 			
 	}
 //sets method to set the signle pixel to the RGB values
-public static void set(int x-factor, int y-factor, int val) {
+public static void set(int x_factor, int y_factor, int val) {
 		String st = hexVal + Integer.toHexString(val) ;
 		System.out.println(st);
 		//3 bytes per pixel throughout the width
-		index = (y-factor*width+x-factor)*3;
+		index = (y_factor*width+x_factor)*3;
 		int count=0; 
 		int sub1=2; 
 		int sub2=4;
